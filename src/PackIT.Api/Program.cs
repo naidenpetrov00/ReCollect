057e27b3322bada6d4304d9223bcd5a107baf550
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-08-20 21:08:25 +0300

    Refactoring

--- src/PackIT.Api/Program.cs
+++ src/PackIT.Api/Program.cs
@@ -1,49 +1,49 @@
 namespace PackIT.Api
 {
-	using PackIT.Api.Infrastructure.Exceptions;
-	using PackIT.Infrastructure;
-	using PackIT.Application;
+    using System.Reflection;
+    using Microsoft.Extensions.DependencyInjection;
+    using PackIT.Api.Infrastructure.Exceptions;
+    using PackIT.Application;
+    using PackIT.Infrastructure;
 
-	using Microsoft.Extensions.DependencyInjection;
-	using System.Reflection;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
 
-			// Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<ExceptionMiddleware>();
 
-			builder.Services.AddScoped<ExceptionMiddleware>();
+            builder.Services.AddApplicationServices();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
 
-			builder.Services.AddApplicationServices();
-			builder.Services.AddInfrastructureServices(builder.Configuration);
+            var app = builder.Build();
 
-			var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+            //app.UseErrorHandling();
+            app.UseHttpsRedirection();
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStaticFiles();
 
-			//app.UseErrorHandling();
-			app.UseHttpsRedirection();
-			app.UseMiddleware<ExceptionMiddleware>();
+            app.UseAuthorization();
 
-			app.UseAuthorization();
+            app.MapControllers();
 
-
-			app.MapControllers();
-
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
