d61789e9e50a0179e0264a254d031408b62e2272
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-10-17 17:45:53 +0300

    Fix the docker_compose

--- src/ReCollect.React/ReCollect.React.Server/Program.cs
+++ src/ReCollect.React/ReCollect.React.Server/Program.cs
@@ -28,9 +28,14 @@ public class Program
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
+            System.Console.WriteLine("Development");
             await app.InitializeDatabaseAsync();
             app.UseSwagger();
             app.UseSwaggerUI();
+        }
+        else
+        {
+            System.Console.WriteLine("Production");
         }
 
         app.MapEndpoints();
